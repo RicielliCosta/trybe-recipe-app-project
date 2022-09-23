c7441549b6e69d31315bb9e88a6e0cf49c0a45dc
PedroSantana <phsantana99@gmail.com>
2022-09-23 19:44:36 -0300

    requisito 2 ao 6 finalizados

--- src/App.js
+++ src/App.js
@@ -3,15 +3,25 @@ import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Meals from './components/Meals';
+// import Header from './components/Header';
 
 function App() {
   return (
     <div>
       <h1>App de receitas</h1>
+      {/* <Header /> */}
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={ Login } />
           <Route exact path="/meals" component={ Meals } />
+          {/* <Route exact path="/drinks" component={ Meals } />
+          <Route exact path={`/meals${id}` } component={ Meals } />
+          <Route exact path={`/drinks${id}` } component={ Meals } />
+          <Route exact path={`/meals${id}/in-progress` } component={ Meals } />
+          <Route exact path={`/drinks${id}/in-progress` } component={ Meals } />
+          <Route exact path="/profile" component={ Meals } />
+          <Route exact path="/done-recipes" component={ Meals } />
+          <Route exact path="/favorites-recipes" component={ Meals } /> */}
         </Switch>
       </BrowserRouter>
     </div>
