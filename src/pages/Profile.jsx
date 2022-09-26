e5f608b5bf334be69d7fffc52eec6646b3ce7b77
PedroSantana <phsantana99@gmail.com>
2022-09-26 19:17:12 -0300

    requisitos 16 a 18 finalizados

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,8 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useContext, useEffect } from 'react';
 import Header from '../components/Header';
 import RecipesContext from '../context/RecipesContext';
+import Footer from '../components/Footer';
 
 function Profile() {
   const { setShowSearchButton, setPageTitle } = useContext(RecipesContext);
@@ -14,6 +16,7 @@ function Profile() {
     <div>
       <Header />
       Profile
+      <Footer />
     </div>
   );
 }
