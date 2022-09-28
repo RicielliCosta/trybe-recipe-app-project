59f406f9b6ede8961786771a47b3d6fa0bd9a529
PedroSantana <phsantana99@gmail.com>
2022-09-28 17:26:10 -0300

    requisitos finalizados, faltam apenas os testes

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,24 +1,63 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useContext, useEffect } from 'react';
+import propTypes from 'prop-types';
 import Header from '../components/Header';
 import RecipesContext from '../context/RecipesContext';
 import Footer from '../components/Footer';
 
-function Profile() {
+function Profile({ history: { push } }) {
   const { setShowSearchButton, setPageTitle } = useContext(RecipesContext);
+  const { email } = JSON.parse(localStorage.getItem('user'));
 
   useEffect(() => {
     setShowSearchButton(false);
     setPageTitle('Profile');
   }, []);
 
+  const onClickLogoutButton = () => {
+    localStorage.clear();
+    push('/');
+  };
+
   return (
     <div>
       <Header />
-      Profile
+      <div>
+        <h2>Profile Page</h2>
+        <span data-testid="profile-email">{ email }</span>
+
+        <button
+          type="button"
+          onClick={ () => push('/done-recipes') }
+          data-testid="profile-done-btn"
+        >
+          Done Recipes
+        </button>
+
+        <button
+          type="button"
+          onClick={ () => push('/favorite-recipes') }
+          data-testid="profile-favorite-btn"
+        >
+          Favorite Recipes
+        </button>
+
+        <button
+          type="button"
+          onClick={ onClickLogoutButton }
+          data-testid="profile-logout-btn"
+        >
+          Logout
+        </button>
+
+      </div>
       <Footer />
     </div>
   );
 }
+
+Profile.propTypes = {
+  history: propTypes.shape({ push: propTypes.func.isRequired }).isRequired,
+};
 
 export default Profile;
